2e4654c103e1f3fa66e7229b01a496a2fcd0d096
Marwane Kalam-Alami <marwane.ka@gmail.com>
2019-06-10 11:36:42 +0200

    Integrate Spotify's Authorization Code Flow

--- README.md
+++ README.md
@@ -12,14 +12,18 @@ This is a web based remote/commander for spotify. It works with the Spotify web 
 ![Screenshot](/example.png?raw=true "Screen shot")
 
 ## Setting up the authorization
-[TODO]
 
-## Run locally (Not up to date!)
+- Go to https://developer.spotify.com/dashboard/applications
+- Create a client ID and whitelist the URL at which you will serve the app (for development: `http://localhost:3000/`)
+- Copy `config.env.default` to `config.env` and set the client ID and secret + your app URL
+
+## Run locally 
 - Install node packages, `$ npm install` (in root folder)
 - Install client packages (in client folder, `client/app`)
   - Run npm `$ npm install`
   - Run build `$ npm run build`
-- Run server `$ node .`
+- Run server (in root folder) `$ npm start`
+- Go to `http://localhost:3000` to authorize and start controlling your account
 
 ## Deploy
 - Copy `ecosystem.json.default` to `ecosystem.json` and set ip-address, username, etc
--- server/server.js
+++ server/server.js
@@ -1,27 +1,40 @@
 const express = require('express');
-const app = express();
-
 const path = require('path');
-
+const fs = require('fs');
+const dotenv = require('dotenv');
+const envfile = require('envfile');
 const SpotifyWebApi = require('spotify-web-api-node');
 
-const config = {
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI,
-    accessToken: process.env.ACCESS_TOKEN,
-    refreshToken: process.env.REFRESH_TOKEN
-}
+dotenv.config({ path: 'config.env' });
 
 /* Initalize the Spotify API */
 var spotifyApi = new SpotifyWebApi({
-    clientId : config.clientId,
-    clientSecret : config.clientSecret,
-    redirectUri : config.redirectUri
-  });
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+    refreshToken: process.env.REFRESH_TOKEN
+});
 
-spotifyApi.setAccessToken(config.accessToken);
-spotifyApi.setRefreshToken(config.refreshToken);
+function isSpotifyAuthenticated() {
+    return !!spotifyApi.getRefreshToken();
+}
+
+function assertSpotifyAuthenticated(res) {
+    if (!isSpotifyAuthenticated()) {
+        res.status(503);
+        res.end();
+        throw new Error('Spotify not authenticated yet');
+    }
+}
+
+function saveEnv() {
+    fs.writeFileSync('./config.env', envfile.stringifySync({
+        CLIENT_ID: process.env.CLIENT_ID,
+        CLIENT_SECRET: process.env.CLIENT_SECRET,
+        REDIRECT_URI: process.env.REDIRECT_URI,
+        REFRESH_TOKEN: spotifyApi.getRefreshToken()
+    }));
+}
 
 /**
  * Refresh the Spotify access token
@@ -29,29 +42,32 @@ spotifyApi.setRefreshToken(config.refreshToken);
  */
 function refresh(callback) {
     spotifyApi.refreshAccessToken()
-    .then(function(data) {
-      console.log('The access token has been refreshed!');
-  
-      // Save the access token so that it's used in future calls
-      spotifyApi.setAccessToken(data.body['access_token']);
-      console.log(data.body['access_token'])
-      callback.call();
-    }, function(err) {
-      console.log('Could not refresh access token', err);
+    .then(function (data) {
+        console.log('The access token has been refreshed');
+        
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+        callback.call();
+    }, function (err) {
+        console.log('Could not refresh access token', err);
+        spotifyApi.setRefreshToken('');
     });
 }
+
+const app = express();
 
 /**
  * Get the current playing song information
  */
-app.get('/api/currentSong', function(req,res,next) {
+app.get('/api/currentSong', function (req, res, next) {
+    assertSpotifyAuthenticated(res);
     spotifyApi.getMyCurrentPlayingTrack()
-    .then(function(data) {
+    .then(function (data) {
         res.send(data);
         // res.send(200);
-    }, function(err) {
+    }, function (err) {
         console.log(err);
-        refresh(function() {
+        refresh(function () {
             res.redirect('/api/currentSong');
         });
     });
@@ -60,8 +76,9 @@ app.get('/api/currentSong', function(req,res,next) {
 /**
  * Control the playback, pause, play, prev, next
  */
-app.get('/api/control/:control', function(req,res,next) {
-    switch(req.params.control) {
+app.get('/api/control/:control', function (req, res, next) {
+    assertSpotifyAuthenticated(res);
+    switch (req.params.control) {
         case 'pause':
             var prom = spotifyApi.pause();
             console.log('pause');
@@ -82,59 +99,82 @@ app.get('/api/control/:control', function(req,res,next) {
             console.log('whatever');
             break;
     }
-    prom.then(function() {
-        res.send({status: 'ok'})
-    }, function(err) {
+    prom.then(function () {
+        res.send({ status: 'ok' })
+    }, function (err) {
         console.log(err);
-        refresh(function() {
+        refresh(function () {
             res.redirect('/api/control/' + req.params.control);
         });
     });
 });
 
-app.get('/api/play/:track', function(req, res, next) {
-    spotifyApi.play({uris: [req.params.track]})
-        .then(function() {
-            res.send({status: 'ok'})
-        }, function(err) {
+app.get('/api/play/:track', function (req, res, next) {
+    assertSpotifyAuthenticated(res);
+    spotifyApi.play({ uris: [req.params.track] })
+        .then(function () {
+            res.send({ status: 'ok' })
+        }, function (err) {
             console.log(err)
             res.send(500)
         })
 })
 
 
-app.get('/api/search/:q', function(req, res, next) {
+app.get('/api/search/:q', function (req, res, next) {
+    assertSpotifyAuthenticated(res);
     spotifyApi.search(req.params.q, ['album', 'artist', 'playlist', 'track'])
-        .then(function(result) {
+        .then(function (result) {
             res.send(result)
-        }, function(err) {
+        }, function (err) {
             console.log(err)
             res.sendStatus(500)
         })
 })
 
+app.get('/', async (req, res, next) => {
+    if (isSpotifyAuthenticated()) {
+        next();
+    } else if (req.query.code) {
+        try {
+            console.log('Received code from spotify, requesting tokens')
+            const authResponse = await spotifyApi.authorizationCodeGrant(req.query.code);
+            spotifyApi.setAccessToken(authResponse.body.access_token);
+            spotifyApi.setRefreshToken(authResponse.body.refresh_token);
+            console.log('Tokens received')
+            saveEnv();
+            res.redirect('/');
+        } catch (e) {
+            console.error(e);
+            res.redirect('/');
+        }
+    } else {
+        console.log('Redirecting to Spotify for authorization')
+        res.redirect(spotifyApi.createAuthorizeURL(['app-remote-control','user-read-playback-state','user-modify-playback-state','user-read-currently-playing']));
+    }
+});
+
 /* The front end */
 app.use(express.static(path.join(__dirname, '../client/app/dist/')));
 
 /* Optional: port on which the server listens */
-if(typeof(process.env.NODE_PORT) !== 'undefined') {
-  port = process.env.NODE_PORT;
+if (typeof (process.env.NODE_PORT) !== 'undefined') {
+    port = process.env.NODE_PORT;
 } else {
-  port = 3000; // Default port
+    port = 3000; // Default port
 }
 
 /* Start the web server */
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('Listening on ' + port);
 });
-  
+
 /* Necessary for some proxy configurations */
 app.enable('trust proxy');
-  
-app.options("/*", function(req, res, next){
+
+app.options("/*", function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.send(200);
 });
-  
